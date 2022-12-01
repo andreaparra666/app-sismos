9c1c1fe2391f6a5b6f373fcabda64b64176eb984
andreaparra666 <andrea.parra.nunez@uabc.edu.mx>
2022-12-01 01:08:14 -0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client';
 
-import '/leaflet/dist/leaflet.css';
-import '/bootstrap/dist/css/bootstrap.min.css';
-import '/font-awesome/css/font-awesome.min.css';
+import 'leaflet/dist/leaflet.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 import App from './App';
 
